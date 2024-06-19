6dda11ed4a9211f39ac2a30f611af631976adf1c
Juan Pablo Saladino <juanpablosaladino@gmail.com>
2024-06-19 12:29:27 -0300

    Se elimina componente momentaneamente

--- src/components/TableButton/table-button.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Props {
-  children: React.ReactNode
-  color: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
-  disabled?: boolean
-  modal: any
-  selectedIdRow?: string
-  onClose: () => void
-}
