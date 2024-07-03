38b06aed0d6efd227f0a07d29c163b9438c57852
Juan Pablo Saladino <juanpablosaladino@gmail.com>
2024-07-03 16:13:15 -0300

    Se modulariza routeo

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css';
-import TurnosPage from './pages/TurnosPage';
-import EspecialistasPage from './pages/EspecialistasPage';
 import AppContextProvider from './context/AppContext';
-import LoginPage from './pages/LoginPage';
+import router from './router/RoutesConfig';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LoginPage />
-  },
-  {
-    path: "/turnos",
-    element: <TurnosPage />
-  },
-  {
-    
-
-  },
-  {
-    path: "/especialistas",
-    element: <EspecialistasPage />
-  },
-  {
-    path: "/login",
-    element:<LoginPage />
-  }
-]);
-
 
 root.render(
   <React.StrictMode>
